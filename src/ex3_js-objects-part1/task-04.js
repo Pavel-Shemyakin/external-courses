c2b5716dc9c73ea6dc64d42ae015ff24b122c1f0
Pavel Shemyakin <paulshemyakin@yandex.com>
2020-04-30 11:49:18 +0300

    Update JS basics homework, exercise 3 part 1

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,8 +1,8 @@
-function checkObject(str, object) {
+function addNewProperty(str, object) {
     if (object.hasOwnProperty(str)) {
         return object
     }
     return Object.assign(object, {[str]: 'new'})
 }
 
-module.exports = checkObject
+module.exports = addNewProperty
